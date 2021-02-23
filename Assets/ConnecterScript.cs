32d1ee0eba5939f65f275f8d2e61ced80aef8baf
epiczain1234 <mukatizain@gmail.com>
2021-02-23 10:05:01 -0500

    Can connect to PUN server

---
+++ Assets/ConnecterScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnecterScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject connect;
+
+    public void connectButton(){
+      if (connect != null){
+         connect.SetActive(true);
+      }
+    }
+}
---
+++ Assets/LobbyManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+
+    [Header("Login UI")]
+    public InputField playerNameInputField;
+    public GameObject UI_LoginGameObject;
+    //
+    //
+    // [Header("Connection Status UI")]
+    // public GameObject ui_ConnectionStatusObject;
+    //
+
+
+
+    #region UNITY Methods
+    void Start()
+    {
+        // uI_LobbyGameObject.SetActive(false);
+        // ui_ConnectionStatusObject.SetActive(false);
+        // UI_LoginGameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    #endregion
+
+
+    #region UI Callback Methods
+    public void OnEnterButtonClicked(){
+      //uI_LobbyGameObject.SetActive(false);
+      // UI_LoginGameObject.SetActive(false);
+      // ui_ConnectionStatusObject.SetActive(true);
+
+         string playerName = playerNameInputField.text;
+
+         if (!string.IsNullOrEmpty(playerName)){
+             PhotonNetwork.LocalPlayer.NickName = playerName;
+             PhotonNetwork.ConnectUsingSettings();
+            if (PhotonNetwork.IsConnected){
+                 Debug.Log("Connected to server");
+
+
+           }
+
+         }
+         else{
+           Debug.Log("Null or Empty player name");
+         }
+    }
+    #endregion
+
+
+    #region PHOTON Callback Methods
+    public override void OnConnected()
+    {
+        Debug.Log("We are connected to the Internet");
+
+        // base.OnConnected();
+
+    }
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log(PhotonNetwork.LocalPlayer.NickName + " is connected to the server");
+    }
+    #endregion
+
+
+
+
+
+
+}
