5e68a9103ee5e2485726931b5660aff7e9aac6f7
epiczain1234 <mukatizain@gmail.com>
2021-03-01 10:38:09 -0500

    added debugging statements

--- Assets/Rooms/CreateRoom.cs
+++ Assets/Rooms/CreateRoom.cs
@@ -7,25 +7,31 @@ using UnityEngine.SceneManagement;
 
 public class CreateRoom : MonoBehaviourPunCallbacks
 {
-  // [SerializedField]
-  // private Text _roomName;
-  // Add modular room name in create funciton
-
+ //  TODO: setup random room name generation for multiple concurrent room instance
   public void OnClick_CreateRoom(){
     RoomOptions options = new RoomOptions();
     options.MaxPlayers = 4;
-    PhotonNetwork.JoinOrCreateRoom("basic", options, TypedLobby.Default);
+    string randomRoomName = "Room" + Random.Range(0, 1000);
+    PhotonNetwork.JoinOrCreateRoom(randomRoomName, options, TypedLobby.Default);
   }
 
   public override void OnCreatedRoom(){
 //    MasterManager.DebugConsole.AddText("Created Room Successfully", this);
-    Debug.Log("Room created");
+  //  Debug.Log("Room created");
     SceneManager.LoadScene("Actual");
+    Debug.Log("The current client has created a room and joined it");
+  }
 
+  public override void OnJoinedRoom(){
+    Debug.Log(PhotonNetwork.NickName + " joined " + PhotonNetwork.CurrentRoom.Name + " Player Count is now " + PhotonNetwork.CurrentRoom.PlayerCount);
   }
 
   public override void OnCreateRoomFailed(short returnCode, string message){
     //  MasterManager.DebugConsole.AddText("Room Creation Failed " + , this);
     Debug.Log("Room Creation Failed");
   }
+  public override void OnPlayerEnteredRoom(Player newPlayer){
+    Debug.Log(newPlayer.NickName + " Joined " + PhotonNetwork.CurrentRoom.Name + " Player Count is now " + PhotonNetwork.CurrentRoom.PlayerCount);
+  }
+
 }
