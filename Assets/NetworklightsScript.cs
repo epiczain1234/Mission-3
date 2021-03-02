f6d79e1d760204130274e0a37ac375d430e11c31
epiczain1234 <mukatizain@gmail.com>
2021-03-02 12:27:45 -0500

    Started work on network lights

---
+++ Assets/NetworklightsScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class NetworklightsScript : MonoBehaviour, IPunObservable
+{
+    // Start is called before the first frame update
+    Rigidbody rb;
+    PhotonView photonView;
+
+    private void awake(){
+      rb = GetComponent<Rigidbody>();
+      photonView = GetComponent <PhotonView>();
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info){
+
+      if (stream.IsWriting){
+
+      }
+      if (stream.IsReading){
+
+      }
+
+    }
+    [PunRPC]
+    public void ChangeColorToGreen(){
+
+    }
+}
