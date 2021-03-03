129bb8eb6ef118eb670d6c152cb645514cce0ee1
epiczain1234 <mukatizain@gmail.com>
2021-03-03 13:52:07 -0500

    Synchronized color change with unity button

--- Assets/NetworklightsScript.cs
+++ Assets/NetworklightsScript.cs
@@ -7,17 +7,27 @@ public class NetworklightsScript : MonoBehaviour, IPunObservable
 {
     // Start is called before the first frame update
     private PhotonView photonView;
-    void Start()
-    {
+    void Start(){
       photonView = GameObject.Find("Sphere").GetComponent<PhotonView>();
+    }
+    // use this function on a ui button (will eventuall move this to a lever)
+    public void ChangeColorOnClick(){
       photonView.RPC("RPC_ChangeColor", RpcTarget.All, null);
     }
 
     [PunRPC]
     void RPC_ChangeColor(){
         Debug.Log("Color change code executed");
-        GetComponent<Renderer>().material.color = Color.red;
+        if (GetComponent<Renderer>().material.color == Color.red){
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+
+        else{
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+
     }
+    // essetial interface component
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info){
 
       if (stream.IsWriting){
