60e6f02934631f13bdf585160491362f009fb95b
epiczain1234 <mukatizain@gmail.com>
2021-03-07 15:11:49 -0500

    Added timer using Time.delta time and added Ready up scene

---
+++ Assets/CountDownTimer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CountDownTimer : MonoBehaviour
+{
+    // Start is called before the first frame
+    float currentTime;
+    float startingTime = 30f;
+    [SerializeField] Text countdownText;
+    void Start()
+    {
+       currentTime = startingTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       currentTime -= 1 * Time.deltaTime;
+       countdownText.text = currentTime.ToString();
+       if (0 >= currentTime){
+         SceneManager.LoadScene("WinScene");
+       }
+    }
+}
---
+++ Assets/ReadyButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Realtime;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+
+public class ReadyButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void On_Click_LoadSceneOrGiveError(){
+        if (PhotonNetwork.CurrentRoom.PlayerCount == 2){
+            SceneManager.LoadScene("MultiplayerRoom");
+        }
+        else{
+          Debug.Log("Not Enough Players to start game");
+          // display pop-up message that there are not enought players
+
+        }
+    }
+}
