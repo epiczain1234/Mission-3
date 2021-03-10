e85ba3641bb7bf9f754aa48e0d9b8b5563965160
epiczain1234 <mukatizain@gmail.com>
2021-03-10 14:06:08 -0500

    Multiplayer Red Light random script working:

--- Assets/TurnSpheresRed.cs
+++ Assets/TurnSpheresRed.cs
@@ -11,6 +11,10 @@ public class TurnSpheresRed : MonoBehaviour, IPunObservable
     void Start()
     {
       lights = new string[6]{"Sphere","Sphere1","Sphere2","Sphere3","Sphere4","Sphere5"};
+      for (int i = 0; i < 6; i++){
+        photonView = GameObject.Find(lights[i]).GetComponent<PhotonView>();
+        photonView.GetComponent<Renderer>().material.color = Color.green;
+      }
     }
 
     // Update is called once per frame
@@ -19,17 +23,23 @@ public class TurnSpheresRed : MonoBehaviour, IPunObservable
       int green = 0;
       for (int i = 0; i < 6; i++){
         photonView = GameObject.Find(lights[i]).GetComponent<PhotonView>();
-        if (GetComponent<Renderer>().material.color == Color.green){
+        if (photonView.GetComponent<Renderer>().material.color == Color.green){
             green++;
+            Debug.Log("we now have " + green + " green speheres");
+        }
+        else{
+          Debug.Log("I guess sphere " + i + " is actually " + photonView.GetComponent<Renderer>().material.color);
         }
 
       }
       if (green == 6){
+          Debug.Log("attempting to grab a sphere and chance it color");
           // generate rangdom Integer
           int grab = Random.Range(0, 6);
           // set the array index sphere correlating to the int to red
+          Debug.Log("changing the color of " + lights[grab] + " now");
           photonView = GameObject.Find(lights[grab]).GetComponent<PhotonView>();
-          GetComponent<Renderer>().material.color = Color.red;
+          photonView.GetComponent<Renderer>().material.color = Color.red;
       }
     }
 
