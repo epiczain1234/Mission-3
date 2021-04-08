6ab7b42a4264929070dabc17c4540f4ee53d1a9e
epiczain1234 <mukatizain@gmail.com>
2021-04-08 17:00:51 -0400

    Added Strike UI

---
+++ Assets/StrikeCounter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
+
+public class StrikeCounter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] Text strikes;
+    void Start() 
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((int)PhotonNetwork.CurrentRoom.CustomProperties["Strikes"] == 1){
+            strikes.text = "X";
+        }
+        if ((int)PhotonNetwork.CurrentRoom.CustomProperties["Strikes"] == 2){
+            strikes.text = "X X";
+        }
+        if ((int)PhotonNetwork.CurrentRoom.CustomProperties["Strikes"] == 3){
+            strikes.text = "X X X ";
+        }
+    }
+}
